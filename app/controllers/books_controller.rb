96f93692948f6730df5736182f5535f1d67c6e22
HayatoKoizumi <129759853+HayatoKoizumi@users.noreply.github.com>
2023-05-04 07:43:35 +0000

    [Add]投稿一覧画面にいいね機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,12 +46,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
   def ensure_correct_user
     book = Book.find(params[:id])
     unless book.user.id == current_user.id
       redirect_to books_path
     end
   end
-  
+
 end
