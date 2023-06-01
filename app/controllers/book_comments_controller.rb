abd28dbf41073dcb07ccba1edb35eb4c0b8964ae
HayatoKoizumi <129759853+HayatoKoizumi@users.noreply.github.com>
2023-06-01 18:00:37 +0000

    応用課題6解説とサンプルコードを参照し復習

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,14 +6,16 @@ class BookCommentsController < ApplicationController
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     @book_comment.save
-    #redirect_to request.referer
+    # app/views/book_comments/create.js.erbを参照する
+    # redirect_to request.referer（非同期通信化により削除）
   end
 
   def destroy
     @book_comment = BookComment.find(params[:id]).destroy
     @book_comment.destroy
     @book = Book.find(params[:book_id])
-    #redirect_to request.referer
+    # app/views/book_comments/destroy.js.erbを参照する
+    # redirect_to request.referer（非同期通信化により削除）
   end
 
 
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,16 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    #redirect_back (fallback_location: books_path)
+    # app/views/favorites/create.js.erbを参照する
+    # redirect_back (fallback_location: books_path) （非同期通信化により削除）
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    #redirect_back (fallback_location: books_path)
+    # app/views/favorites/destroy.js.erbを参照する
+    # redirect_back (fallback_location: books_path) （非同期通信化により削除）
   end
 
 end
