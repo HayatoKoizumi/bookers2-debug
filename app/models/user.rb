3f0c21ef2c21ae9a8d863abde4535cfbb16dfe72
HayatoKoizumi <129759853+HayatoKoizumi@users.noreply.github.com>
2023-07-19 16:28:52 +0000

    [Update]DM機能　途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
