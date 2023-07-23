01e6f25de2cd348426d7a04050fcaad5b140a165
HayatoKoizumi <129759853+HayatoKoizumi@users.noreply.github.com>
2023-07-23 16:34:58 +0000

    [Update]投稿数の前日比/前週比を表示 途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
     @book = Book.new
+    @books = @user.books.page(params[:page]).reverse_order
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
     if @user.id == current_user.id
