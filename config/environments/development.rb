cef5d175c04ba51ab1f78fb88917c51071104243
HayatoKoizumi <129759853+HayatoKoizumi@users.noreply.github.com>
2023-08-02 16:44:51 +0000

    [Update]グループメンバーへのメール送信機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,14 +75,14 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "01dcb29c657b47fe8bea46f70507c49a.vfs.cloud9.ap-northeast-1.amazonaws.com"
-  
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'gmail.com',
-    user_name:            '送信元となるGmailアドレスを記入',
-    password:             'アプリパスワードを記入',
+    user_name:            'sample@sample',
+    password:             '123456',
     authentication:       'login',
     enable_starttls_auto:  true
   }
